2acbc6c543c5b8778cd6d559c6a7d41564c6bea5
Suin Jeong <jungsuin8512@gmail.com>
2023-08-16 16:11:37 +0700

    Change data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Banner from "./../assets/images/banner.jpg";
+import Banner from "./../assets/images/banner_university.png";
 
 const Header = () => {
   return (
     <div className="flex flex-col text-white w-full h-fit bg-gray-500 rounded-2xl overflow-hidden relative">
       
       <div className="px-8 py-10 h-full flex flex-col justify-center z-[2]">
-        <p className="font-bold text-[1.5rem] w-full">Fire Fress</p>
-        <p className="">Fress & Healthy Food</p>
+        <p className="font-bold text-[1.5rem] w-full">Politeknik Negeri Jember Cafeteria</p>
+        <p className="">Sweet & Delicious Food</p>
       </div>
       <div className="bg-black w-full h-full flex absolute z-[1] opacity-50"></div>
       <img className="object-cover w-full h-full absolute bg-yellow-400" src={Banner} alt="Banner" />
--- src/models/Dessert.js
+++ src/models/Dessert.js
@@ -1,29 +1,47 @@
-import Dessert from './../assets/images/dessert.png'
-import Dessert1 from './../assets/images/dessert1.png'
-import Dessert2 from './../assets/images/dessert2.png'
+import Dessert from './../assets/images/Fantasy-Apel.png'
+import Dessert1 from './../assets/images/Fantasy-Jeruk.png'
+import Dessert2 from './../assets/images/mochi.png'
+import Dessert3 from './../assets/images/Ketan-HItam.png'
+import Dessert4 from './../assets/images/Kacang-Hijau.png'
+
 
 const ListDessert = [
     {
         id: 301,
-        name: "Strawberry Dessert Cake",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dignissimos perspiciatis non, amet animi nam voluptatum officia alias",
+        name: "Fantasy Apel",
+        desc: "Ice with apple flavor can be enjoyed by the whole family. The delicious and refreshing taste of apples can make your body and throat feel fresh in hot weather",
         image: Dessert,
-        price: 25000
+        price: 5000
     },
     {
         id: 302,
-        name: "Strawberry Pudding",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dignissimos perspiciatis non, amet animi nam voluptatum officia alias",
+        name: "Fantasy Jeruk",
+        desc: "Ice with Orange flavor can be enjoyed by the whole family. The delicious and refreshing taste of apples can make your body and throat feel fresh in hot weather",
         image: Dessert1,
-        price: 20000
+        price: 5000
     },
     {
         id: 303,
-        name: "StrawberryChoco Cake",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dignissimos perspiciatis non, amet animi nam voluptatum officia alias",
+        name: "mochi",
+        desc: "Mochi ice cream is a popular Japanese dessert made from a sweet pounded rice dough wrapped around ice cream.",
         image: Dessert2,
-        price: 32000
+        price: 7000
     },
+    {
+        id: 304,
+        name: "Hula Hula Tape Ketan Hitam",
+        desc: "delicious and fresh black sticky rice flavored ice cream covered in chocolate with a sweet taste that will make you addicted if you eat it.",
+        image: Dessert3,
+        price: 6000
+    },
+    {
+        id: 305,
+        name: "Hula Hula Kacang Hijau",
+        desc: "delicious and fresh green bean flavored ice cream covered in chocolate with a sweet taste that will make you addicted when you eat it",
+        image: Dessert4,
+        price: 6000
+    },
+    
     
 ]
 
